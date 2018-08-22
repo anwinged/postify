c5a6d8fc532cc2bd43e1ce7dc6eb0f89f22cc353
Anton Vakhrushev <a.vakhrushev@timeweb.ru>
2018-08-22 17:42:02 +0300

    Add current url detection

--- postify.js
+++ postify.js
@@ -1,1 +1,17 @@
-console.log('Yeah!')
+console.log('Load!');
+
+function onGot(tabInfo) {
+	var tab = tabInfo[0];
+	var url = tab.url;
+  	console.log(url);
+}
+
+function onError(error) {
+	console.log(`Error: ${error}`);
+}
+
+function sendCurrentUrl() {
+	browser.tabs.query({currentWindow: true, active: true}).then(onGot, onError);
+}
+
+browser.browserAction.onClicked.addListener(sendCurrentUrl);
